4432cbe698450a289742e9bcf863e112b5479f6d
Gwins7 <gwins777@qq.com>
2023-03-07 12:39:18 +0800

    add l2fwd_queue

---
+++ chisel_dev/l2fwd-queue/build/.main.o.d
@@ -0,0 +1,195 @@
+dep_main.o = /home/huangtianyi/dpdk-stable-19.11.6/examples/l2fwd-queue/main.c \
+ /usr/include/stdc-predef.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_config.h \
+ /usr/include/stdio.h /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/stddef.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/stdarg.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h /usr/include/stdlib.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/byteswap-16.h \
+ /usr/include/xlocale.h /usr/include/sys/types.h /usr/include/time.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/sigset.h /usr/include/bits/time.h \
+ /usr/include/sys/sysmacros.h /usr/include/bits/pthreadtypes.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib-float.h \
+ /usr/include/string.h /usr/include/bits/string.h \
+ /usr/include/bits/string2.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/wchar.h /usr/include/inttypes.h \
+ /usr/include/sys/queue.h /usr/include/netinet/in.h \
+ /usr/include/sys/socket.h /usr/include/sys/uio.h /usr/include/bits/uio.h \
+ /usr/include/bits/socket.h /usr/include/bits/socket_type.h \
+ /usr/include/bits/sockaddr.h /usr/include/asm/socket.h \
+ /usr/include/asm-generic/socket.h /usr/include/asm/sockios.h \
+ /usr/include/asm-generic/sockios.h /usr/include/bits/in.h \
+ /usr/include/setjmp.h /usr/include/bits/setjmp.h /usr/include/ctype.h \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/getopt.h /usr/include/signal.h /usr/include/bits/signum.h \
+ /usr/include/bits/siginfo.h /usr/include/bits/sigaction.h \
+ /usr/include/bits/sigcontext.h /usr/include/bits/sigstack.h \
+ /usr/include/sys/ucontext.h /usr/include/bits/sigthread.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/stdbool.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_common.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include-fixed/limits.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_config.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/lib/librte_eal/linux/eal/include/rte_os.h \
+ /usr/include/sched.h /usr/include/bits/sched.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_log.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_compat.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_malloc.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_memory.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_fbarray.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_rwlock.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/generic/rte_rwlock.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_atomic.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/emmintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/xmmintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/mmintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/mm_malloc.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/generic/rte_atomic.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_atomic_64.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_pause.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/generic/rte_pause.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_spinlock.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/generic/rte_spinlock.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_lcore.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_per_lcore.h \
+ /usr/include/pthread.h /usr/include/bits/timex.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_eal.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_bus.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_dev.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_pci_dev_feature_defs.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_launch.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_rtm.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_cpuflags.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/generic/rte_cpuflags.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_branch_prediction.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_common.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_pause.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_cycles.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/generic/rte_cycles.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_debug.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_log.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_branch_prediction.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_memcpy.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_vect.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/generic/rte_vect.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/x86intrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/ia32intrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/immintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/pmmintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/tmmintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/smmintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/popcntintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/wmmintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/fxsrintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/xsaveintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/xsaveoptintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/xsavesintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/xsavecintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avxintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx2intrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512fintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512erintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512pfintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512cdintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vlintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512bwintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512dqintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vlbwintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vldqintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512ifmaintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512ifmavlintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vbmiintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vbmivlintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx5124fmapsintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx5124vnniwintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vpopcntdqintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vbmi2intrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vbmi2vlintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vnniintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vnnivlintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512vpopcntdqvlintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/avx512bitalgintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/shaintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/lzcntintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/bmiintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/bmi2intrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/fmaintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/f16cintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/rtmintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/xtestintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/cetintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/gfniintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/vaesintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/vpclmulqdqintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/movdirintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/sgxintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/pconfigintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/waitpkgintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/cldemoteintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/rdseedintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/prfchwintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/adxintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/clwbintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/clflushoptintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/wbnoinvdintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/pkuintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/mm3dnow.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/fma4intrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/ammintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/xopintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/lwpintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/tbmintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/mwaitxintrin.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/9.3.0/include/clzerointrin.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_cycles.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_prefetch.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/generic/rte_prefetch.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_interrupts.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_eal_interrupts.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_random.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_ether.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_mbuf.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_mempool.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_spinlock.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_ring.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_memzone.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_ring_generic.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_byteorder.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/generic/rte_byteorder.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_byteorder_64.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_mbuf_ptype.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_mbuf_core.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/generic/rte_atomic.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_ethdev.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_devargs.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_errno.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_dev_info.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_eth_ctrl.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_flow.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_arp.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_icmp.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_ip.h \
+ /usr/include/netinet/ip.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_sctp.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_tcp.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_udp.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_esp.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_higig.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_mbuf_dyn.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/build/include/rte_ethdev_core.h \
+ /home/huangtianyi/dpdk-stable-19.11.6/examples/l2fwd-queue/../../drivers/net/qdma/rte_pmd_qdma.h \
+ /usr/include/linux/pci.h /usr/include/linux/pci_regs.h
---
+++ chisel_dev/l2fwd-queue/toeplitz_test.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+pthread_mutex_t print_lock;
+
+unsigned int sw_hash(unsigned int s[],unsigned int hash_seed,unsigned int hash_mask){
+	unsigned int sw_result = 0;
+	int i,k;
+	for (k=0;k<3;k++)
+		for (i=0;i<32;i++){
+            if (((s[k] << i) & 0x80000000) == 0x80000000) sw_result ^= hash_seed;
+            hash_seed = (hash_seed << 1) | (hash_seed >> 31);
+	}
+	return sw_result & hash_mask;
+}
+
+void data_atoi(char *s, unsigned int *result){
+    // result[3];s[24]
+    int i;
+    for (i=0;i<24;i++,s++){
+        int index = i/8;
+        int cur = (*s >= 'a')?(10+*s-'a'):(*s-'0');
+        result[index] = (result[index] << 4) + cur;
+    }
+    return;
+}
+
+int pthread_process(unsigned int queue_id){
+    char filename[12];
+    snprintf(filename,12,"dump_log_%d",queue_id);
+
+    unsigned int t[3],r[3];
+    char tmp[26]; //actual-use = 24
+    int cmp_fail = 0;
+    int i,t_res,r_res;
+    FILE *fd = fopen(filename,"r");
+    fseek(fd, 0, SEEK_SET);
+    while(fread(tmp,25,1,fd)!=0) {
+        for (i=0;i<3;i++){
+            t[i]=0;r[i]=0;
+        }
+        data_atoi(tmp,t);
+        r[0]=t[1];
+        r[1]=t[0];
+        r[2]=(t[2] << 16) | (t[2] >> 16);
+        t_res = sw_hash(t,0x6d5a6d5a,0xf);
+        r_res = sw_hash(r,0x6d5a6d5a,0xf);
+        if (t_res != r_res || t_res != queue_id) {
+            cmp_fail = 1;
+            goto done;
+        }
+    }
+done:
+    fclose(fd);
+    pthread_mutex_lock(&print_lock);
+    printf("queue %2d's test result %s!\n",queue_id,cmp_fail?"fail":"success");
+    if (cmp_fail) {
+        printf("info: (t_res = %2d,r_res = %2d)\n",t_res,r_res);
+        printf("t[3] = %08x %08x %08x\nr[3] = %08x %08x %08x\n",t[0],t[1],t[2],r[0],r[1],r[2]);
+    }
+    pthread_mutex_unlock(&print_lock);
+    return cmp_fail;
+}
+
+void main(void){
+    int thread_num = 16;
+    int i,res,thread_res[16];
+    pthread_t *handler = malloc(thread_num * sizeof(pthread_t));
+    pthread_mutex_init(&print_lock,NULL);
+    for (i=0;i<thread_num;i++){
+        pthread_create(&handler[i],NULL,pthread_process,i);
+    }
+    for (i=0;i<thread_num;i++){
+        pthread_join(handler[i],&thread_res[i]);
+    }
+    // printf("[Overall] test finished!\n");
+    res = 0;
+    for (i=0;i<thread_num;i++) res |= thread_res[i];
+    if (res)  printf("[Overall] test fail!\n");
+    else printf("[Overall] test success!\n");
+    return;
+}
